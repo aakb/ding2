d6838ecb4bd24b1f2e8eb167aa515c2e93628367
Jesper Kristensen <cableman@linuxdev.dk>
2021-04-15 11:21:06 +0200

    AAKBET-735: Added excel export

---
+++ modules/aakb_writing_competition/templates/aakb-writing-competition-export.tpl.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @file
+ * Default template for the module.
+ */
+?>
+
+<p><?php echo _aakb_writing_competition_t('Number of contributions: !num', ['!num' => $count]) ?></p>
+<?php echo render($link); ?>
